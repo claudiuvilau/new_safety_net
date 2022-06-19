ed2ddd1d15efa709ff84a4963fc03d3e6217fbae
Claudiu Vilau <vilau_claudiu@yahoo.com>
2022-06-19 09:26:28 +0200

    update interface

--- src/main/java/com/openclassrooms/new_safety_net/repository/PersonsRepository.java
+++ src/main/java/com/openclassrooms/new_safety_net/repository/PersonsRepository.java
@@ -8,4 +8,40 @@ import com.openclassrooms.new_safety_net.model.Persons;
 @Repository
 public interface PersonsRepository extends CrudRepository<Persons, String> {
 
+    /*
+     * http://localhost:8080/person
+     * Cet endpoint permettra d’effectuer les actions suivantes via Post/Put/Delete
+     * avec HTTP :
+     * ● ajouter une nouvelle personne ;
+     * ● mettre à jour une personne existante (pour le moment, supposons que le
+     * prénom et le nom de
+     * famille ne changent pas, mais que les autres champs peuvent être modifiés) ;
+     * ● supprimer une personne (utilisez une combinaison de prénom et de nom comme
+     * identificateur
+     * unique).
+     */
+
+    void getPerson(String nomperson);
+
+    /*
+     * http://localhost:8080/firestation
+     * Cet endpoint permettra d’effectuer les actions suivantes via Post/Put/Delete
+     * avec HTTP :
+     * ● ajout d'un mapping caserne/adresse ;
+     * ● mettre à jour le numéro de la caserne de pompiers d'une adresse ;
+     * ● supprimer le mapping d'une caserne ou d'une adresse.
+     */
+
+    /*
+     * http://localhost:8080/medicalRecord
+     * Cet endpoint permettra d’effectuer les actions suivantes via Post/Put/Delete
+     * HTTP :
+     * ● ajouter un dossier médical ;
+     * ● mettre à jour un dossier médical existant (comme évoqué précédemment,
+     * supposer que le
+     * prénom et le nom de famille ne changent pas) ;
+     * ● supprimer un dossier médical (utilisez une combinaison de prénom et de nom
+     * comme
+     * identificateur unique)
+     */
 }
